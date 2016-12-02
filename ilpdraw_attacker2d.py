301dad329e9fe31e3f517cc2e166dc29da02953b
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-12-02 12:37:43 +0000

    When drawing the attacker's path default to the minimum source distance on the x axis

--- ilpdraw_attacker2d.py
+++ ilpdraw_attacker2d.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 
+import argparse
 import sys
 
 import matplotlib.pyplot as plt
+import networkx as nx
 
 from results.parser import Results
 
 results_name = sys.argv[1]
 
-r = Results(results_name)
+parser = argparse.ArgumentParser(description="ILP Draw Attacker 2D", add_help=True)
+parser.add_argument("results")
+parser.add_argument("--with-node-id", action='store_true', default=False)
+
+args = parser.parse_args(sys.argv[1:])
+
+r = Results(args.results)
+
+sources = {1}
 
 moves = []
 
-for (time, pair) in enumerate(r.attacker_moves_at_time):
-	if time == 0:
-		moves.append((pair[0], time))
+if not args.with_node_id:
+    shortest_path_lengths = {src: nx.shortest_path_length(r.graph, source=src) for src in sources}
 
-	if pair[0] != pair[1]:
-		moves.append((pair[1], time))
+    def min_src_distance(nid):
+        return min(dist[nid] for dist in shortest_path_lengths.values())
+
+for (time, pair) in enumerate(r.attacker_moves_at_time):
+    if time == 0:
+        xvalue = pair[0] if args.with_node_id else min_src_distance(pair[0])
+
+        moves.append((xvalue, time))
+
+    if pair[0] != pair[1]:
+        xvalue = pair[1] if args.with_node_id else min_src_distance(pair[1])
+
+        moves.append((xvalue, time))
 
 print(moves)
 
@@ -31,11 +51,16 @@ ax.plot(x, t, label="attacker path")
 ax.scatter(x, t, c='b', marker='o')
 ax.legend()
 
-ax.set_xlabel("Node ID")
-ax.set_ylabel("time")
+if args.with_node_id:
+    ax.set_xlabel("Node ID")
+    ax.set_xticks(r.results.neighbours.keys())
+else:
+    ax.set_xlabel("Minimum Source Distance (hops)")
+    ax.set_xlim(left=0)
 
+ax.set_ylabel("Time (seconds)")
 ax.set_ylim(bottom=0)
-ax.set_xticks(r.results.neighbours.keys())
+
 
 #plt.savefig('this.png')
 
